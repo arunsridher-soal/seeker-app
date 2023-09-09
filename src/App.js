b8c1b79a7953c696c2a1fe686c0dbe93dce7733b
arunsridher <arunsridher@gmail.com>
2023-09-09 18:34:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import io from "socket.io-client";
 
+const socket = io("https://api.dsep.samagra.io");
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("socket connected");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("socket disconnected");
+    });
+
+    socket.on("response", (payload) => {
+      console.log("ccc payload in response: ", payload);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("response");
+    };
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
