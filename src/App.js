3ed07815a0673263e8bc48bcbfdf588b4615e77b
arunsridher <arunsridher@gmail.com>
2023-09-10 17:47:11 +0530

    updated endpoint and code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import { useEffect } from "react";
 import "./App.css";
-import io from "socket.io-client";
 
-const socket = io("https://dsep-protocol-client.becknprotocol.io");
 function App() {
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("socket connected");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      context: {
+        domain: "dsep:courses",
+        action: "search",
+        version: "1.0.0",
+        bap_uri: "https://dsep-protocol-network.becknprotocol.io/",
+        bap_id: "dsep-protocol.becknprotocol.io",
+      },
+      message: {
+        intent: {
+          item: {
+            descriptor: {
+              name: "python",
+            },
+          },
+        },
+      },
     });
 
-    socket.on("disconnect", () => {
-      console.log("socket disconnected");
-    });
-
-    socket.on("response", (payload) => {
-      console.log("ccc payload in response: ", payload);
-    });
-
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("response");
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
     };
+
+    fetch(
+      "https://dsep-protocol-client.becknprotocol.io/search",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   }, []);
   return <div className="App"></div>;
 }
