879ccc2272e1ccd9ea35e02cb9a754f2d5260b46
arunsridher <arunsridher@gmail.com>
2023-09-11 20:27:09 +0530

    removed content-type header

--- src/App.js
+++ src/App.js
@@ -3,40 +3,27 @@ import "./App.css";
 
 function App() {
   useEffect(() => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var raw = JSON.stringify({
-      context: {
-        domain: "dsep:courses",
-        action: "search",
-        version: "1.0.0",
-        bap_uri: "https://dsep-protocol-network.becknprotocol.io/",
-        bap_id: "dsep-protocol.becknprotocol.io",
-      },
-      message: {
-        intent: {
-          item: {
-            descriptor: {
-              name: "python",
+    fetch("https://dsep-protocol-client.becknprotocol.io/search", {
+      method: "POST",
+      body: {
+        context: {
+          domain: "dsep:courses",
+          action: "search",
+          version: "1.0.0",
+          bap_uri: "https://dsep-protocol-network.becknprotocol.io/",
+          bap_id: "dsep-protocol.becknprotocol.io",
+        },
+        message: {
+          intent: {
+            item: {
+              descriptor: {
+                name: "python",
+              },
             },
           },
         },
       },
-    });
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://dsep-protocol-client.becknprotocol.io/search",
-      requestOptions
-    )
-      .then((response) => response.text())
+    })
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
   }, []);
